841ffce37779641fb7452c80aae188b3b6e76335
JoaoVictorRS <joaososteam@gmail.com>
2025-12-06 23:15:11 -0300

    Bismuth Block and Bismuth Ore add

--- src/main/java/net/jao/tutorialmod/TutorialMod.java
+++ src/main/java/net/jao/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.jao.tutorialmod;
 
+import net.jao.tutorialmod.block.ModBlocks;
 import net.jao.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -37,6 +38,7 @@ public class TutorialMod {
         NeoForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -54,6 +56,12 @@ public class TutorialMod {
 
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS ){
             event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
         }
 
     }
